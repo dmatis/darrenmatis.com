b8077fe662f4c8605ce174a655f295652ef9d1db
Darren Matis-Mei <dmatismei@microsoft.com>
2023-07-10 15:13:46 -0700

    cleaned up landing page

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,12 @@ import Link from 'next/link';
 
 function Home() {
   return (
-    <>
-      <h1 className="title">I'm Darren</h1>
-      <ol>
-        <li>
-          <Link href="/about">About Me</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact Me</Link>
-        </li>
-      </ol>
-    </>
+    <main>
+      <h1>I'm Darren</h1>
+      <p>
+        <Link href="/contact">Contact Me</Link>
+      </p>
+    </main>
   );
 }
 
